b9d07824c916080647cd316eca22af891f7bf45b
LucasPF1 <lucaspfernandes1997@hotmail.com>
2021-09-29 11:51:19 -0300

    tarefa_3_4_Projeto

---
+++ src/main/java/org/grupo7/conquerdaring/ConquerdaringApplication.java
@@ -0,0 +1,13 @@
+package org.grupo7.conquerdaring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConquerdaringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConquerdaringApplication.class, args);
+	}
+
+}
