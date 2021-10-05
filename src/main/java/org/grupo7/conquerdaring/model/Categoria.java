b6ca8a420907f46683783c272a643df0bb87bbe4
Fábio Antunes <fabio.fao03@gmail.com>
2021-10-05 15:27:45 -0300

    Task 6

--- src/main/java/org/grupo7/conquerdaring/model/Categoria.java
+++ src/main/java/org/grupo7/conquerdaring/model/Categoria.java
@@ -1,12 +1,21 @@
 package org.grupo7.conquerdaring.model;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -24,13 +33,23 @@ public class Categoria {
 	@Size(min = 3 , max = 200,  message = "O atributo nomeOrganizacao é obrigatório, deve conter no minimo 03 e no maximo 200 caracteres")	
 	private String nomeOrganizacao;
 	
-	@NotNull
-	@Size(min = 3 , max = 50, message = "O atributo estudante é obrigatório, deve conter no minimo 03 e no maximo 50 caracteres")	
-	private String estudante;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new java.sql.Date(System.currentTimeMillis());
 	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public long getId() {
 		return id;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 
 	public void setId(long id) {
@@ -53,11 +72,12 @@ public class Categoria {
 		this.nomeOrganizacao = nomeOrganizacao;
 	}
 
-	public String getEstudante() {
-		return estudante;
+	public Date getDate() {
+		return date;
 	}
 
-	public void setEstudante(String estudante) {
-		this.estudante = estudante;
+	public void setDate(Date date) {
+		this.date = date;
 	}
+	
 }
