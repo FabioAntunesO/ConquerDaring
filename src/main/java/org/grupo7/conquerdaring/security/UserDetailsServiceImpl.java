a32316091a4655a307149f100a98bb3f406b9b8a
Fábio Antunes <fabio.fao03@gmail.com>
2021-10-13 11:07:45 -0300

    pacote security

---
+++ src/main/java/org/grupo7/conquerdaring/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package org.grupo7.conquerdaring.security;
+
+import java.util.Optional;
+
+import org.grupo7.conquerdaring.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UsuarioRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		Optional<Usuario> user = userRepository.findByUsuario(userName);
+		user.orElseThrow(() -> new UsernameNotFoundException(userName + " not found."));
+		return user.map(UserDetailsImpl::new).get();
+	}
+}
